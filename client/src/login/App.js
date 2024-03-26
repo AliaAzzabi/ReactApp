388a81b01c620902ed76d3f8a83275a7a438857f
AliaAzzabi <122538793+AliaAzzabi@users.noreply.github.com>
2024-03-26 16:43:36 +0100

    ajouter une page login

---
+++ client/src/login/App.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import './style.css'; 
+
+
+function App() {
+  const [isSignUpMode, setIsSignUpMode] = useState(false);
+
+  const handleSignUpClick = () => {
+    setIsSignUpMode(true);
+  };
+
+  const handleSignInClick = () => {
+    setIsSignUpMode(false);
+  };
+
+  return (
+    <div className={`container ${isSignUpMode ? 'sign-up-mode' : ''}`}>
+      <div className="forms-container">
+        <div className="signin-signup">
+          <form action="#" className="sign-in-form">
+            <h2 className="title">Sign in</h2>
+            <div className="input-field">
+              <i className="fas fa-user"></i>
+              <input type="text" placeholder="Username" />
+            </div>
+            <div className="input-field">
+              <i className="fas fa-lock"></i>
+              <input type="password" placeholder="Password" />
+            </div>
+            <input type="submit" value="Login" className="btn solid" />
+            <p className="social-text">Or Sign in with social platforms</p>
+            <div className="social-media">
+              {/* Social media icons */}
+            </div>
+          </form>
+          <form action="#" className="sign-up-form">
+            {/* Sign-up form fields */}
+          </form>
+        </div>
+      </div>
+
+      <div className="panels-container">
+        <div className="panel left-panel">
+          <div className="content">
+            <h3>New here ?</h3>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis,
+              ex ratione. Aliquid!
+            </p>
+            <button className="btn transparent" onClick={handleSignUpClick}>
+              Sign up
+            </button>
+          </div>
+          <img src="/login.png" className="image" alt="" />
+
+        </div>
+        <div className="panel right-panel">
+          <div className="content">
+            <h3>One of us ?</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+              laboriosam ad deleniti.
+            </p>
+            <button className="btn transparent" onClick={handleSignInClick}>
+              Sign in
+            </button>
+          </div>
+          <img src="/home-right.png" className="image" alt="" />
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
