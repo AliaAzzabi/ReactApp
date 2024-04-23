bbe773299f307f44980348f1a6c5016cc3337c6f
AliaAzzabi <122538793+AliaAzzabi@users.noreply.github.com>
2024-04-23 12:26:22 +0200

    page de connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 
 import Sidebar from "../src/global/Sidebar"
@@ -41,48 +41,48 @@ function App() {
   const shouldDisplaySidebarTopbar = !excludeSidebarTopbarPaths.includes(location.pathname);
 
   return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-
-        <div className="app">
-          {/* Afficher le Sidebar et Topbar en fonction de la condition */}
-          {shouldDisplaySidebarTopbar && <Sidebar isSidebar={isSidebar} />}
-          <main className="content">
-            {shouldDisplaySidebarTopbar && <Topbar setIsSidebar={setIsSidebar} />}
-            <Routes>
-            <Route path="/se_connecter" element={<Login/>}/>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/doctorList" element={<DoctorList/>}/>
-              <Route path="/adddoctor" element={<AddDoctor/>}/>
-              <Route path="/assitantList" element={<AssitantList/>}/>
-              <Route path="/addAssistant" element={<AddAide/>}/>
-              <Route path="/departList" element={<DepartList/>}/>
-              <Route path="/adddepart" element={<Adddepart/>}/>
-              <Route path="/specialityList" element={<SpecialiteList/>}/>
-              <Route path="/addSpeciality" element={<Addsp/>}/>
-              <Route path="/modifMedecin" element={<ModifMed/>}/>
-              <Route path="/modifAide" element={<ModifAide/>}/>
-              <Route path="/modifDepart" element={<ModifDepart/>}/>
-              <Route path="/modifSpecialiti" element={<ModifSpecialiti/>}/>
-            
-              <Route path="/patientList" element={<PatientList/>}/>
-              <Route path="/addPatient" element={<AddPatient/>}/>
-           
-              <Route path="/facture" element={<Facture/>}/>
-
-              <Route path="/payementList" element={<PayementList/>}/>
-              <Route path="/addPayement" element={<AddPayement/>}/>
-              <Route path="/salle-d'attente" element={<Salle/>}/>
-              <Route path="/rendez-vous" element={<RendezVous/>}/>
-              <Route path="/patient" element={<Patients/>}/>
-                 
-                                      
-            </Routes>
-          </main>
-        </div>
-      </ThemeProvider>
-    </ColorModeContext.Provider>
+    <>
+      {location.pathname !== "/se_connecter" && (
+        <ColorModeContext.Provider value={colorMode}>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <div className="app">
+              {/* Afficher le Sidebar et Topbar en fonction de la condition */}
+              {shouldDisplaySidebarTopbar && <Sidebar isSidebar={isSidebar} />}
+              <main className="content">
+                {shouldDisplaySidebarTopbar && <Topbar setIsSidebar={setIsSidebar} />}
+                <Routes>
+                  <Route path="/" element={<Dashboard />} />
+                  <Route path="/doctorList" element={<DoctorList/>}/>
+                  <Route path="/adddoctor" element={<AddDoctor/>}/>
+                  <Route path="/assitantList" element={<AssitantList/>}/>
+                  <Route path="/addAssistant" element={<AddAide/>}/>
+                  <Route path="/departList" element={<DepartList/>}/>
+                  <Route path="/adddepart" element={<Adddepart/>}/>
+                  <Route path="/specialityList" element={<SpecialiteList/>}/>
+                  <Route path="/addSpeciality" element={<Addsp/>}/>
+                  <Route path="/modifMedecin" element={<ModifMed/>}/>
+                  <Route path="/modifAide" element={<ModifAide/>}/>
+                  <Route path="/modifDepart" element={<ModifDepart/>}/>
+                  <Route path="/modifSpecialiti" element={<ModifSpecialiti/>}/>
+                  <Route path="/patientList" element={<PatientList/>}/>
+                  <Route path="/addPatient" element={<AddPatient/>}/>
+                  <Route path="/facture" element={<Facture/>}/>
+                  <Route path="/payementList" element={<PayementList/>}/>
+                  <Route path="/addPayement" element={<AddPayement/>}/>
+                  <Route path="/salle-d'attente" element={<Salle/>}/>
+                  <Route path="/rendez-vous" element={<RendezVous/>}/>
+                  <Route path="/patient" element={<Patients/>}/>
+                </Routes>
+              </main>
+            </div>
+          </ThemeProvider>
+        </ColorModeContext.Provider>
+      )}
+      <Routes>
+        <Route path="/se_connecter" element={<Login/>}/>
+      </Routes>
+    </>
   );
 }
 
