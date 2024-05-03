05d0ccb5cd01b1202c76c0eb691a3157de8c8497
AliaAzzabi <122538793+AliaAzzabi@users.noreply.github.com>
2024-05-03 16:01:47 +0200

    projrt

--- projetpfe/client/src/pages/ListeMedecin.jsx
+++ projetpfe/client/src/pages/ListeMedecin.jsx
@@ -124,13 +124,7 @@ function ListeMedecin() {
                 image: formData.get('image'),
                 specialite: selectedSpecialite,
                 departement: selectedDepartement,
-<<<<<<< HEAD
-                
-=======
-                image: formData.get('image'),
->>>>>>> a4c5710f3c1a27b6538aca618b478ded2901c90d
-                
-            };
+                     };
             console.log('updatedMedecin:', updatedMedecin); // Add this line
             UpdateMedecin(selectedMedecin._id, updatedMedecin, (res) => {
                 if (res.data) {
