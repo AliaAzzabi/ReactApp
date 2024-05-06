2a739ffd7f443a24e4b2b313a25cfb5bf8a15afe
NesrineBoukraa <129533946+NesrineBoukraa@users.noreply.github.com>
2024-05-06 15:14:16 +0200

    changer

--- server/specialité/specialitiesSchema.js
+++
@@ -1,9 +0,0 @@
-// specialitySchema.js
-const mongoose = require("mongoose");
-
-const SpecialtiesSchema = new mongoose.Schema({
-    nom: String,
-    description: String,
-});
-
-module.exports = mongoose.model("Specialtie", SpecialtiesSchema);
